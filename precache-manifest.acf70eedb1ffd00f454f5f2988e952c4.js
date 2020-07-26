37d1061d0b8518b885b7cfc6b14bac03dde2a2e1
martynaskadisa <8273932+martynaskadisa@users.noreply.github.com>
2020-07-26 23:40:35 +0300

    Updates

---
+++ precache-manifest.acf70eedb1ffd00f454f5f2988e952c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241f16afaf6091601537c0a02c772141",
+    "url": "/use-media-session/index.html"
+  },
+  {
+    "revision": "5aba1ff799c18bfc95cf",
+    "url": "/use-media-session/static/css/main.97e3bcc1.chunk.css"
+  },
+  {
+    "revision": "d8198b3e29ada6c17733",
+    "url": "/use-media-session/static/js/2.dcfcd98c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/use-media-session/static/js/2.dcfcd98c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aba1ff799c18bfc95cf",
+    "url": "/use-media-session/static/js/main.4095ced3.chunk.js"
+  },
+  {
+    "revision": "ff0b81db93810ae912c3",
+    "url": "/use-media-session/static/js/runtime-main.2e039491.js"
+  }
+]);
